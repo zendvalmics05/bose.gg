26296016beafeea96d7945fdd308198ade0ca4a8
Sagnik Bose <sagnikbose2005dcfan@gmail.com>
2024-10-27 13:56:22 +0530

    Streamlined the file structure to increase the ease of scalability.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
